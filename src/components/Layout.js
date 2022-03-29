9618ae33bd4479ce6792f04184306335168e6f7e
Markus Müller <markus@MBP-von-Markus.fritz.box>
2022-03-29 15:45:22 +0200

    favicon and github added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,5 @@
 import Box from '@mui/material/Box';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import SyncButton from './SyncButton';
 import { Link } from 'react-router-dom';
 
@@ -13,6 +14,9 @@ function Layout({ children, backgroundColor = 'inherit' }) {
             </div>
             <div className="column">
               <SyncButton />
+              <a href="https://github.com/teztok/8bidou-demo">
+                <GitHubIcon />
+              </a>
             </div>
           </div>
         </div>
