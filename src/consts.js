3a0dbabcedbe79e3e6a8359534881fb74a3da2b4
Simon Kusterer <simon@sope.io>
2022-05-01 15:13:27 +0200

    make sure PIXEL_FORMAT is a number

--- src/consts.js
+++ src/consts.js
@@ -1,7 +1,7 @@
 // 8x8 color
 export const FA2_CONTRACT = process.env.REACT_APP_FA2_CONTRACT || 'KT1MxDwChiDwd6WBVs24g1NjERUoK622ZEFp';
 export const MARKETPLACE_CONTRACT = process.env.REACT_APP_MARKETPLACE_CONTRACT || 'KT1BvWGFENd4CXW5F3u4n31xKfJhmBGipoqF';
-export const PIXEL_FORMAT = process.env.REACT_APP_PIXEL_FORMAT || 8;
+export const PIXEL_FORMAT = process.env.REACT_APP_PIXEL_FORMAT ? parseInt(process.env.REACT_APP_PIXEL_FORMAT, 10) : 8;
 export const COLOR_FORMAT = process.env.REACT_APP_COLOR_FORMAT || 'hex';
 export const EVENT_TYPE_PREFIX = process.env.REACT_APP_EVENT_TYPE_PREFIX || '8BID_8X8_COLOR';
 
